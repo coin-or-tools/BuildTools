c2c1120dbdd8f64178b404818e80d34bc0a7f9f7
JP Fasano <jpfasano@gmail.com>
2007-10-17 13:54:18 +0000

    Modified functions called to implement running command in seperate process

--- NBosCommand.py
+++ NBosCommand.py
@@ -8,39 +8,37 @@ import subprocess
 # Return: command's return code, stdout messages, & stderr messages
 #------------------------------------------------------------------------
 def run(cmd) :
-#  p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-#  cmdRc=p.wait
-#  cmdRc=p.returncode
-#  print "run rc="
-#  print cmdRc
-#  cmdStdout=p.stdout.read()
-#  cmdStderr=p.stderr.read()
-#  retVal = { 'returnCode':cmdRc, 'stdout':cmdStdout, 'stderr':cmdStderr }
-#  return retVal 
+  p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+  cmdStdout,cmdStderr=p.communicate()
+  cmdRc=p.returncode
+  print "run rc="
+  print cmdRc
+  retVal = { 'returnCode':cmdRc, 'stdout':cmdStdout, 'stderr':cmdStderr }
+  return retVal 
 
 
-  run = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-  # Wait for the process to return
-  import thread, threading
-  out, err = [''], ['']
-  #out, err = "", "" 
-  out_ended, err_ended = threading.Event(), threading.Event()
-
-  def getOutput(output, lines, ended_event) :
-    #for i in output.readlines() : lines.append(i)
-    for i in output.readlines() : 
-      lines[0] = lines[0]+i
-    ended_event.set()
-
-  out_thread = thread.start_new_thread(getOutput, (run.stdout, out, out_ended))
-  err_thread = thread.start_new_thread(getOutput, (run.stderr, err, err_ended))
-
-  out_ended.wait()
-  err_ended.wait()
-
-  returncode = run.wait()
-
-  retVal = { 'returnCode':returncode, 'stdout':out[0], 'stderr':err[0] }
-  return retVal
+#  run = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#
+#  # Wait for the process to return
+#  import thread, threading
+#  out, err = [''], ['']
+#  #out, err = "", "" 
+#  out_ended, err_ended = threading.Event(), threading.Event()
+#
+#  def getOutput(output, lines, ended_event) :
+#    #for i in output.readlines() : lines.append(i)
+#    for i in output.readlines() : 
+#      lines[0] = lines[0]+i
+#    ended_event.set()
+#
+#  out_thread = thread.start_new_thread(getOutput, (run.stdout, out, out_ended))
+#  err_thread = thread.start_new_thread(getOutput, (run.stderr, err, err_ended))
+#
+#  out_ended.wait()
+#  err_ended.wait()
+#
+#  returncode = run.wait()
+#
+#  retVal = { 'returnCode':returncode, 'stdout':out[0], 'stderr':err[0] }
+#  return retVal
 
