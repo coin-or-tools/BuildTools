e0427783e299257700f77068f25c72b337c12fe3
JP Fasano <jpfasano@gmail.com>
2007-10-21 16:01:29 +0000

    Added function to determine latest stable build number of a project.

--- NBsvnCommand.py
+++ NBsvnCommand.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
 import os
+import urllib2
+import re
 
 import NBlogMessages
 import NBemail
@@ -22,3 +24,25 @@ def run(svnCmd,dir,project) :
     NBemail.sendCmdMsgs(project,result,svnCmd)
     retVal='Error'
   return retVal
+
+
+#------------------------------------------------------------------------
+# Function which returns the latest stable version of a project
+#------------------------------------------------------------------------
+def latestStableVersion(project) :
+  url='https://projects.coin-or.org/svn/'+project+'/stable'
+  handle=urllib2.urlopen(url)
+  html=handle.read()
+  handle.close()
+
+  # In html code find the latest version number
+  #   <li><a href="3.2/">3.2/</a></li>
+  #   <li><a href="3.3/">3.3/</a></li>
+  r=r'<li><a href="(\d*\.\d*)/">(\d*\.\d*)/</a></li>'
+  findResult=re.findall(r,html)
+  latestStableVersionRepeated2Times = findResult[-1:][0]
+  latestStableVersion=latestStableVersionRepeated2Times[0]
+  return latestStableVersion
+  
+  
+  
