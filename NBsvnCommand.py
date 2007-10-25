124ac55ad332747f1b0a4fd9f02c7eef45debbf6
JP Fasano <jpfasano@gmail.com>
2007-10-25 15:54:07 +0000

    Added two new functions.  svnRevision(url) returns the Revision number for svn copy at url  newer(source,target) returns true if target should be rebuilt (ie source newer than target).  The expectation is that source is the coin-or svn repository and target is on the local machines file system. There is no need to rebuild and retest if nothing has changed.  This function checks externals to see if any externals have changed requiring a rebuild.

--- NBsvnCommand.py
+++ NBsvnCommand.py
@@ -3,6 +3,7 @@
 import os
 import urllib2
 import re
+import sys
 
 import NBlogMessages
 import NBemail
@@ -44,6 +45,82 @@ def latestStableVersion(project) :
   latestStableVersionRepeated2Times = findResult[-1:][0]
   latestStableVersion=latestStableVersionRepeated2Times[0]
   return latestStableVersion
+
+#------------------------------------------------------------------------
+# Return svn revision number or url
+# If not found the return -1
+#------------------------------------------------------------------------
+def svnRevision(url) :
+  retVal=-1
+  result = NBosCommand.run('svn info '+url)
+  if result['returnCode']==0 :
+    reg=r'Revision: (\d+)'
+    found=re.findall(reg,result['stdout'])
+    if len(found)!=0 :
+      retVal=int(found[0])
+  return retVal
   
+
+#------------------------------------------------------------------------
+# newer(source, target)
+# Return true if source and is more recently modified than target,
+# (ie return true if source is newer than target and target needs
+# to be rebuilt).
+#
+# If either sourrce or target don't exist then true is returned.
+#
+#------------------------------------------------------------------------
+def newer(source,target) :
+
+  #print '------------------------'
+  #print source
+  #print target
+  #print '------------------------'
+  #print ' '
   
-  
+  tarRev=svnRevision(target)
+  if tarRev==-1 :
+    # Target probably does not exist. It does not have an svn revision
+    # nubmer, so return that it is out of date.
+    return True
+
+  srcRev=svnRevision(source)
+  if srcRev==-1 :
+    # Source should exist. Something is wrong that will be caught
+    # when an 'svn checkout' is done.
+    return True
+
+  if srcRev>tarRev :
+    return True
+
+  # if there is an externals file then process it
+  extFileName=os.path.join(target,"Externals")
+  if os.path.isfile(extFileName) :
+    extFilePtr = open(extFileName, "r")
+    line = extFilePtr.readline()
+
+    while line:
+      line=line.strip()
+      if line!='' :
+        if line[0]!='#':
+          reg=r'(\S+)(\s+)(\S+)'
+          found=re.findall(reg,line)
+          if len(found)!=1:
+            # something is wrong. Do a rebuild
+            return True
+          found=found[0]
+          if len(found)!=3 :
+            # something is wrong. Do a rebuild
+            return True
+          extTarget=os.path.join(target,found[0])
+          extSource=found[2]
+          # Recursive call to see if external indicates rebuild
+          if newer(extSource,extTarget) :
+            extFilePtr.close()
+            return True
+      line = extFilePtr.readline()
+
+    extFilePtr.close()
+
+  return False
+
