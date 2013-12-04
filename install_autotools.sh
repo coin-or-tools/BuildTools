393123299d6778b13d5423018d06d46ae6b7ca1e
Stefan Vigerske <svigerske@gams.com>
2013-12-04 20:51:50 +0000

    merge r2552 and r2554 from autotools-update branch

---
+++ install_autotools.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# script to download and install the autoools versions that we currently use with COIN-OR/BuildTools
+# original script by Pierre Bonami
+
+PREFIX=$HOME/local2
+
+acver=2.69
+amver=1.12
+ltver=2.4.2
+
+# exit immediately if something fails
+set -e
+
+# so that we can configure automake with the new (then installed) autoconf
+export PATH=$PREFIX/bin:$PATH
+
+# cleanup from previous (maybe failed) build
+rm -rf autoconf-$acver* automake-$amver* libtool-$ltver*
+
+wget ftp://ftp.gnu.org/gnu/autoconf/autoconf-$acver.tar.gz
+tar xvzf autoconf-$acver.tar.gz
+cd autoconf-$acver
+./configure --prefix=$PREFIX
+make install
+cd ..
+
+wget ftp://ftp.gnu.org/gnu/automake/automake-$amver.tar.gz
+tar xvzf automake-$amver.tar.gz
+cd automake-$amver
+./configure --prefix=$PREFIX
+make install
+cd ..
+
+wget ftp://ftp.gnu.org/gnu/libtool/libtool-$ltver.tar.gz
+tar xvzf libtool-$ltver.tar.gz
+cd libtool-$ltver
+./configure --prefix=$PREFIX
+make install
+cd ..
+
+rm -rf autoconf-$acver* automake-$amver* libtool-$ltver*
