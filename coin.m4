coin-or-tools/BuildTools

commit f1c2a6497e8caa14dbd3bf7660fbc1fc11f261e6

Running dos2unix regardless of platform due to same bug now arising on MSys2

coin.m4
@@ -1846,7 +1846,6 @@ AC_DEFUN([AC_COIN_PROG_LIBTOOL],
   case $build in
     *-mingw*)
       CYGPATH_W=echo
-      mydos2unix=
       ;;
   esac
 

commit 117dd29d8808aacff6f1255bcfe8ca0b18448edd

only pipe through dos2unix if it exists
- should fix issue introduced on msys with f1c2a649

coin.m4
@@ -1827,7 +1827,10 @@ AC_DEFUN([AC_COIN_PROG_LIBTOOL],
   AC_SUBST(ac_cxx_preproc_warn_flag)
 
   AC_MSG_NOTICE([Build is "$build".])
-  mydos2unix='| dos2unix'
+  AC_CHECK_PROG([dos2unix], [dos2unix], [dos2unix])
+  if test "$dos2unix" = dos2unix ; then
+    mydos2unix="| dos2unix"
+  fi
   case $build in
     *-mingw*)
       CYGPATH_W=echo